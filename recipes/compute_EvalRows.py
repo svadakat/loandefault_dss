eaa979b2ea3f74b967dcacc0ac465be0cabf74a8
admin <admin>
2019-10-29 16:04:24 -0400

    Saved recipe: LOANDEFAULTNEW.compute_EvalRows

---
+++ recipes/compute_EvalRows.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+import dataiku
+import pandas as pd, numpy as np
+from dataiku import pandasutils as pdu
+
+# Read recipe inputs
+loandefaulteval1_joined_prepared = dataiku.Dataset("loandefaulteval1_joined_prepared")
+loandefaulteval1_joined_prepared_df = loandefaulteval1_joined_prepared.get_dataframe()
+
+
+# Compute recipe outputs from inputs
+# TODO: Replace this part by your actual code that computes the output, as a Pandas dataframe
+# NB: DSS also supports other kinds of APIs for reading and writing data. Please see doc.
+
+evalRows_df = loandefaulteval1_joined_prepared_df # For this sample code, simply copy input to output
+
+
+# Write recipe outputs
+evalRows = dataiku.Dataset("EvalRows")
+evalRows.write_with_schema(evalRows_df)
