bff8be9bf689a81a4b966dea13fb29d734947302
Suresh <Suresh>
2020-01-27 09:48:28 -0500

    Saved recipe: LOANDEFAULTNEW.compute_test

---
+++ recipes/compute_test.r
@@ -0,0 +1,12 @@
+library(dataiku)
+
+# Recipe inputs
+lendingclub_training_prepared <- dkuReadDataset("Lendingclub_training_prepared", samplingMethod="head", nbRows=100000)
+
+# Compute recipe outputs from inputs
+# TODO: Replace this part by your actual code that computes the output, as a R dataframe or data table
+test <- lendingclub_training_prepared # For this sample code, simply copy input to output
+
+
+# Recipe outputs
+dkuWriteDataset(test,"test")
